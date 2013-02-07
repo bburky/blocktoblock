83efcb36bfecba082d280c1c1b51c1c35cc6da46
Blake Burkhart <bburky@bburky.com>
2013-02-07 07:29:13 -0600

    html2svg2canvas working and copying css for it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-r = new Renderer({canvas: document.getElementById('canvas')})
+r = new Renderer({canvas: document.getElementById('canvas')});
+
+var $p1 = $($("p")[1]);
+var $p2 = $p1.clone();
+$p2.copyCSS($p1);
+console.log($p2);
+r.renderHTML($p2[0].outerHTML);
